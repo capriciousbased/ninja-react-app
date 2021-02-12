6ea37e9d08edde3f1cb576719856b3186fd85fe4
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-02-12 11:58:47 +0100

    reusing custiom hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import Home from "./Home";
-import Navbar from "./Navbar";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Create from "./Create";
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   return (
     <Router>
-      <div className="app">
+      <div className="App">
         <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-        </Switch>
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,16 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  const { data: blogs, isPending, error } = useFetch(
+  const { error, isPending, data: blogs } = useFetch(
     "http://localhost:8000/blogs"
   );
+
   return (
     <div className="home">
-      {error && <div> {error} </div>}
-      {isPending && <div> Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 };
