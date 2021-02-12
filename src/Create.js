80dd1e1b51a44f7838ee6f05a6aaa2906a7f107c
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-02-12 13:20:54 +0100

    add forms

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,41 @@
+import { useState } from "react";
 const Create = () => {
-    return (
-      <div className="create">
-        <h2>Add a New Blog</h2>
-      </div>
-    );
+  const [title, setTitle] = useState(" ");
+  const [body, setBody] = useState(" ");
+  const [author, setAuthor] = useState("mario");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const blog = { title, body, author };
+    console.log(blog);
   }
-   
-  export default Create;
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label> Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label> Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label> Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="marion">mario</option>
+          <option value="yoshi">Yoshi</option>
+        </select>
+        <button>Add Blog </button>
+      </form>
+    </div>
+  );
+};
+
+export default Create;
